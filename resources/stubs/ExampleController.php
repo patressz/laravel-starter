b775a861427656c510d8047f520d41e6d9164953
Patrik Strišovský <patrik.strisovsky7@gmail.com>
2023-10-31 11:23:07 +0100

    wip

--- resources/stubs/ExampleController.php
+++ resources/stubs/ExampleController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ExampleController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View
     {
         return view('pages.admin.example.index');
     }
